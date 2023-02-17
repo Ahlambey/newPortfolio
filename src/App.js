1bd6d884d913d77c406446fe3ce3cbba07cd37e6
Ahlambey <juleahlam@gmail.com>
2023-02-17 17:53:37 +0100

    Added loading animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
+import { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+import loadingAnimation from "./components/lottie/96189-looping-loading-animation.json";
 import Home from "./components/Home";
-import { useEffect } from "react";
-import * as Realm from "realm-web";
-
 
 function App() {
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const lottieStyle = {
+    width: "250px",
+  };
 
+  useEffect(() => {
+    const onPageLoad = () => {
+      setHasLoaded(true);
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
 
   return (
     <div className="App">
-      <Home/>
+      {hasLoaded ? (
+        <Home/>
+      ) : (
+        <div className="loading">
+          <Lottie
+            animationData={loadingAnimation}
+            loop={true}
+            style={lottieStyle}
+          ></Lottie>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
-
